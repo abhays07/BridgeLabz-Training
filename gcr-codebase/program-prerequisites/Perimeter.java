1340ff004eb514f935b5a46789d2c2dbf10e0e0a
abhays07 <itsrajputabhaysingh997@gmail.com>
2025-12-16 22:50:32 +0530

    [Abhay Singh Lodhi] : Added Perimeter Calculate Program

---
+++ gcr-codebase/program-prerequisites/Perimeter.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Perimeter
+{
+
+public static int perimeter(int l ,int b)
+{
+int Per= 2*(l+b);
+return per ;
+}
+
+public static void main(String args[])
+{
+Scanner sc= new Scanner(System.in);
+System.out.println("Enter the length");
+int l=sc.nextInt();
+System.out.println("Enter the breadth");
+int b=sc.nextInt();
+
+System.out.println(perimeter(l,h)); 
+}
+
+}
