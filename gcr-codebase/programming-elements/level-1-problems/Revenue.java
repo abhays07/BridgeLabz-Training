cda950687c48deb75dfcfbeb51b7559900d44402
abhays07 <itsrajputabhaysingh997@gmail.com>
2025-12-17 13:22:32 +0530

    [Abhay Singh Lodhi] : added calculate profit & profit % Program

---
+++ gcr-codebase/programming-elements/level-1-problems/Revenue.java
@@ -0,0 +1,18 @@
+public class Revenue {
+	public static void calculatePnL(int costPrice, int sellingPrice){
+		
+		int profit = sellingPrice-costPrice;
+		int profitPercentage= (profit*100)/costPrice;
+		
+		System.out.println("The Cost Price is INR : " + costPrice + " and Selling Price is INR " + sellingPrice);
+		System.out.println("The Profit is INR "+ profit + " and the Profit Percentage is "+ profitPercentage);
+		
+	
+	}
+	public static void main(String [] args){
+		int costPrice = 129;
+		int sellingPrice = 191;
+		calculatePnL(costPrice,sellingPrice);
+		
+	}
+ }
