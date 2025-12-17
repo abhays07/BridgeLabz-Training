b9b91181caf58a74c4a0133f472df484bbe81e4f
abhays07 <itsrajputabhaysingh997@gmail.com>
2025-12-17 11:42:05 +0530

    [Abhay Singh Lodhi] : added calculateAge program

---
+++ gcr-codebase/programming-elements/level-1-problems/CalculateAge.java
@@ -0,0 +1,13 @@
+class CalculateAge{
+	public static int age(int b, int c){
+		int currentAge = c-b;
+		return currentAge;
+	}
+	public static void main(String [] args){
+		
+		int birthYear = 2000;
+		int currentYear=2024;
+		
+		System.out.println(age(birthYear, currentYear));
+	}
+}
