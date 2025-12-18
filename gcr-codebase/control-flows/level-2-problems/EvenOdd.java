681cb828be2dfec6362728d7905c4874b7b90860
abhays07 <itsrajputabhaysingh997@gmail.com>
2025-12-18 15:45:45 +0530

    [Abhay Singh Lodhi] : Added Even & Odd Calculate Program

---
+++ gcr-codebase/control-flows/level-2-problems/EvenOdd.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class EvenOdd{
+
+	public static void evenOdd(int n)
+	{
+       for(int i=1;i<=n;i++)
+	   {
+	     if(i%2==0)
+		 {
+		  System.out.println(i + " is an even number"); 
+		 }
+		 else
+		  System.out.println(i + " is an odd number"); 
+
+		 
+	   }
+	 
+	}
+public static void main(String args[])
+{
+
+   Scanner sc=new Scanner(System.in); 
+      System.out.println("Enter the number"); 
+   int number=sc.nextInt();
+   evenOdd(number);
+
+   
+   
+}
+
+}
