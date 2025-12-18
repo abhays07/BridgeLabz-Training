2f279926de63f795b11b13249792f1bc738b59ad
abhays07 <itsrajputabhaysingh997@gmail.com>
2025-12-18 17:19:01 +0530

    [Abhay Singh Lodhi] : Added Find Leap Year calculate Program

---
+++ gcr-codebase/control-flows/level-3-problems/FindLeapYear.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class FindLeapYear {
+	
+	public static void checkLeapYear(){
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		
+		if (year>=1582){
+			if(year % 4 ==0 && year%400==0){
+				System.out.println( year + " is a Leap Year");
+			}
+			else {
+				System.out.println(year+ " is not a Leap Year");
+			}
+		}
+		
+	}
+	
+	public static void main (String [] args){
+		checkLeapYear();
+		
+	}
+}
