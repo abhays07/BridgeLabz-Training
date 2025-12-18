5107ab2b577bad863a7c15b470dd985aeff585ec
abhays07 <itsrajputabhaysingh997@gmail.com>
2025-12-18 15:47:34 +0530

    [Abhay Singh Lodhi] : Added FizzBuzz Program

---
+++ gcr-codebase/control-flows/level-2-problems/FizzBuzz.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class FizzBuzz{
+	
+	public static void  checkMultiples(){
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		if (number>0){
+			 if(number%3 ==0 && number%5 ==0 ){
+				System.out.println("Fizzbuzz");
+			}
+			else if(number%5 ==0){
+				System.out.println("Buzz");
+			}
+			else if(number%3 ==0){
+				System.out.println("Fizz");
+			}
+		}
+		else {
+			System.out.println("Not a multiple of 3 & 5");
+		}
+	}
+	
+	public static void main (String [] args){
+		checkMultiples();
+		
+	}
+}
