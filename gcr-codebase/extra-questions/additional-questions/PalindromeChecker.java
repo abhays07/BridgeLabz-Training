67714064d2bcd350f1fee6d9408eca08710de50d
abhays07 <itsrajputabhaysingh997@gmail.com>
2025-12-24 22:28:48 +0530

    [Abhay Singh Lodhi] : Added the program to check if the string is palindrome or not

---
+++ gcr-codebase/extra-questions/additional-questions/PalindromeChecker.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+
+        String input = takeInput();
+
+        if (isPalindrome(input)) {
+            System.out.println("The string is a Palindrome.");
+        } else {
+            System.out.println("The string is NOT a Palindrome.");
+        }
+    }
+
+    // Function to take input
+    public static String takeInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        return sc.nextLine();
+    }
+
+    // Function to check palindrome
+    public static boolean isPalindrome(String s) {
+
+        String reverse = "";
+
+        // Reversing the string
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reverse = reverse + s.charAt(i);
+        }
+
+        // Comparing original and reversed string
+        return s.equals(reverse);
+    }
+}
