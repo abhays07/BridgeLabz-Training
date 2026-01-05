929af3e625105eda15abfa95bd31319b97a65ded
abhays07 <itsrajputabhaysingh997@gmail.com>
2026-01-05 12:27:51 +0530

    [Abhay Singh Lodhi] : Implement a round-robin CPU scheduling algorithm using a circular linked list. Each node will represent a process and contain Process ID, Burst Time, and Priority. Implemented the add, delete, search and update operations.

---
+++ dsa-practice/gcr-codebase/javaDsaConcept/src/com/linkedlist/circularlinkedlist/roundrobinschedulingalgorithm/ProcessNode.java
@@ -0,0 +1,21 @@
+package com.linkedlist.circularlinkedlist.roundrobinschedulingalgorithm;
+
+public class ProcessNode {
+	int pid;
+    int burstTime;
+    int remainingTime;
+    int priority;
+
+    int waitingTime;
+    int turnaroundTime;
+
+    ProcessNode next;
+
+    public ProcessNode(int pid, int burstTime, int priority) {
+        this.pid = pid;
+        this.burstTime = burstTime;
+        this.remainingTime = burstTime;
+        this.priority = priority;
+        this.next = null;
+    }
+}
