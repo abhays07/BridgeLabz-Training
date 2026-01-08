cd396b186b46a0d0e4edf1990a66bdcfe739ff24
abhays07 <itsrajputabhaysingh997@gmail.com>
2026-01-08 15:37:04 +0530

    [Abhay Singh Lodhi] : Added the Program that uses InputStreamReader to read  binary data from a file and print it as characters.

--- dsa-practice/gcr-codebase/javaDsaConcept/src/com/reader/inputstreamreader/InputStreamReaderDemo.java
+++ dsa-practice/gcr-codebase/javaDsaConcept/src/com/reader/inputstreamreader/InputStreamReaderDemo.java
@@ -1,5 +1,17 @@
 package com.reader.inputstreamreader;
 
-public class InputStreamReaderDemo {
+import java.io.*;
 
+public class InputStreamReaderDemo {
+	public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(new FileInputStream("C:\\Users\\HP\\Desktop\\Java Programming Workspace\\java-dsa-practice\\gcr-codebase\\javaDsaConcepts\\src\\com\\sample.txt"), "UTF-8"));
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+        br.close();
+    }
 }
