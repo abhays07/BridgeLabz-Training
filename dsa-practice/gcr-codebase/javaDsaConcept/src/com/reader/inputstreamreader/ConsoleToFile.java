b0d6936b87c49fd15589e76944448ab75f687f39
abhays07 <itsrajputabhaysingh997@gmail.com>
2026-01-08 15:33:58 +0530

    [Abhay Singh Lodhi] : Added the Program that uses InputStreamReader to read user input from the console and write the input to a file.

--- dsa-practice/gcr-codebase/javaDsaConcept/src/com/reader/inputstreamreader/ConsoleToFile.java
+++ dsa-practice/gcr-codebase/javaDsaConcept/src/com/reader/inputstreamreader/ConsoleToFile.java
@@ -1,5 +1,20 @@
 package com.reader.inputstreamreader;
 
+import java.io.*;
+
 public class ConsoleToFile {
 
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        FileWriter fw = new FileWriter("C:\\Users\\HP\\Desktop\\Java Programming Workspace\\java-dsa-practice\\gcr-codebase\\javaDsaConcepts\\src\\com\\inputstreamreader\\output.txt");
+
+        String input;
+        while (!(input = br.readLine()).equalsIgnoreCase("exit")) {
+            fw.write(input + "\n");
+        }
+
+        fw.close();
+        br.close();
+    }
 }
