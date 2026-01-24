b7b932dedf5b3dadaf893966a9564c37e14e1611
abhays07 <itsrajputabhaysingh997@gmail.com>
2026-01-24 16:33:43 +0530

    [Abhay Singh Lodhi] : Implemented username validation using regular expressions to ensure correct format, including starting with a letter, allowing only alphanumeric characters and underscores, and enforcing length constraints between 5 and 15 characters.

--- java-collections/gcr-codebase/javaCollections/src/com/regex/UsernameValidator.java
+++ java-collections/gcr-codebase/javaCollections/src/com/regex/UsernameValidator.java
@@ -3,7 +3,7 @@ public class UsernameValidator {
     public static void main(String[] args) {
         String username = "user_123";
 
-        // Regex: starts with letter, 5–15 chars, letters/digits/_
+        // Regex:  Starts with letter, 5–15 chars, letters/digits/_
         String regex = "^[A-Za-z][A-Za-z0-9_]{4,14}$";
 
         System.out.println(username.matches(regex));
