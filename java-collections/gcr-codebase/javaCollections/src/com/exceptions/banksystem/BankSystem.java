b46fb37ebfbeec12ca33741029c0c9a7ae6ffa73
abhays07 <itsrajputabhaysingh997@gmail.com>
2026-01-24 12:15:32 +0530

    [Abhay Singh Lodhi] : Developed a Bank Transaction System with robust exception handling using both built-in and custom exceptions. Implemented withdrawal validation to handle insufficient balance and negative amounts, ensuring safe and reliable transaction processing.

---
+++ java-collections/gcr-codebase/javaCollections/src/com/exceptions/banksystem/BankSystem.java
@@ -0,0 +1,17 @@
+package com.exceptions.banksystem;
+
+public class BankSystem {
+    public static void main(String[] args) {
+
+        BankAccount account = new BankAccount();
+
+        try {
+        	
+            account.withdraw(5000);
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
