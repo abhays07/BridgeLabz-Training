da2e5a035d41f75700050a977e3861de89e7b843
abhays07 <itsrajputabhaysingh997@gmail.com>
2026-01-27 12:07:16 +0530

    [Abhay Singh Lodhi] : Implemented a custom @LogExecutionTime annotation to measure and compare method execution times using System.nanoTime and Reflection.

---
+++ java-collections/gcr-codebase/javaCollections/src/com/annotations/intermediatelevel/logexecution/LogExecutionTime.java
@@ -0,0 +1,9 @@
+package com.annotations.intermediatelevel.logexecution;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface LogExecutionTime {
+	
+}
