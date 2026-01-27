a11bf0aaf1ce7e071c2f637822eaa0b03894d046
abhays07 <itsrajputabhaysingh997@gmail.com>
2026-01-27 11:30:28 +0530

    [Abhay Singh Lodhi] : Implemented runtime annotation processing using Reflection to retrieve and display values from a custom @Author annotation applied to a class.

---
+++ java-collections/gcr-codebase/javaCollections/src/com/reflection/intermediatelevel/retrieveanootationsatruntime/Book.java
@@ -0,0 +1,10 @@
+package com.reflection.intermediatelevel.retrieveanootationsatruntime;
+
+
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+
+@Author(name = "Shifa Khan")
+class Book { 
+	
+}
